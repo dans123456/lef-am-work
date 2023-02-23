484da0c600622fe42ade70681679a0cf56f0c505
dans123456 <dansosiaw133@gmail.com>
2023-02-23 17:14:57 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# name = 'danso kwadwo siaw'
+
+#first_name ='danso'
+#last_name = 'siaw'
+#phone_number = 204637772
+#age= 17
+#print( first_name + last_name)
+
+
+name_of_students = ['siaw' ,'danso' ,'yaa']
+print(name_of_students)
+#second_student = name_of_students[1]
+
+# this is when printing  second item
+#print(second_student)
+# when an element to a list it is .append
+name_of_students.append('shira')
+print(name_of_students)
